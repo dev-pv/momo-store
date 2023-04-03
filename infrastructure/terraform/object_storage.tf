25dea49dbd449cfdd70e7cc0d9c9c6f7f6a65db3
Vladimir Prokoshev <vl.prokoshev@gmail.com>
2023-04-03 18:10:25 +0300

    add production helm chart

--- infrastructure/terraform/object_storage.tf
+++ infrastructure/terraform/object_storage.tf
@@ -15,9 +15,16 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   description        = "static access key for object storage"
 }
 
+
 // Создание бакета с использованием ключа
 resource "yandex_storage_bucket" "momo-store-bucket" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   bucket     = "momo-store-bucket"
+}
+
+resource "yandex_storage_bucket" "momo-store-terraform-bucket" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "momo-store-terraform-state-bucket"
 }
---
+++ infrastructure/terraform/sa.tf
@@ -0,0 +1,5 @@
+resource "yandex_iam_service_account" "eso-service-account" {
+  name        = "eso-service-account"
+  description = "eso-service-account"
+  folder_id   = "b1g6f5e97oomo9hnu6o0"
+}
